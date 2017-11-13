4dfb2bab94c0eb02d60a50868469b7097988575a
Rewind <sploing1@gmail.com>
2017-11-13 00:00:00 -0800

    added litty interactivity
    uan

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args)
 	{
-		System.out.println("u a n");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("ian ");
+		String ans = sc.nextLine();
+		System.out.println("uan " + ans);
 	}
 }
